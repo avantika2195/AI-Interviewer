c3659591127e9537cebf05f9fea0eeb7c6e3396c
Avantika Srivastava <srivastavaavantika465@gmail.com>
2025-04-10 03:18:42 +0530

    Fix Clerk middleware error

--- middleware.js
+++ middleware.js
@@ -1,15 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-
-
 const isProtectedRoute = createRouteMatcher([
-    '/dashboard(.*)',
-    '/forum(.*)',
-  ]);
+  "/dashboard(.*)",
+  "/forum(.*)",
+]);
 
-  export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
-  });
+export default clerkMiddleware((auth, req) => {
+  try {
+    if (isProtectedRoute(req)) {
+      auth().protect();
+    }
+  } catch (error) {
+    console.error("Middleware error:", error);
+    return new Response("Middleware Error", { status: 500 });
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
